622b0eaf4efe8d4506f24f8f6dd2f9b7bdf362b1
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2021-11-03 14:03:31 +0100

    Add project files.

---
+++ berek2020/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace berek2020
+{
+  public partial class Form1 : Form
+  {
+    List<adatok> berek = new List<adatok>();
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Shown(object sender, EventArgs e)
+    {
+      Beolvas();
+      Masodikfeladat();
+      Harmadikfeladat();
+    }
+
+    private void Harmadikfeladat()
+    {
+      
+    }
+
+    private void Masodikfeladat()
+    {
+      string ki = $"{berek.Count} fő";
+      lbKimenet.Items.Add(ki);
+    }
+
+    private void Beolvas()
+    {
+     
+        StreamReader be = new StreamReader("berek2020.txt");
+
+        be.ReadLine();
+
+        while (!be.EndOfStream)
+        {
+          string[] a = be.ReadLine().Split(';');
+
+          berek.Add(new adatok(a[0],a[1],a[2],int.Parse(a[3]), int.Parse(a[4])));
+        }
+
+        be.Close();
+      
+    }
+  }
+}
---
+++ berek2020/adatok.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+  class adatok
+  {
+
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+      set { nev = value; }
+    }
+
+    private string neme;
+
+    public string Neme
+    {
+      get { return neme; }
+      set { neme = value; }
+    }
+
+    private string beszerzes;
+
+    public string Beszerzes
+    {
+      get { return beszerzes; }
+      set { beszerzes = value; }
+    }
+
+    private int ev;
+
+    public int Ev
+    {
+      get { return ev; }
+      set { ev = value; }
+    }
+
+    private int fizetes;
+
+    public int Fizetes
+    {
+      get { return fizetes; }
+      set { fizetes = value; }
+    }
+
+    public adatok(string nev, string neme, string beszerzes, int ev, int fizetes)
+    {
+      this.nev = nev;
+      this.neme = neme;
+      this.beszerzes = beszerzes;
+      this.ev = ev;
+      this.fizetes = fizetes;
+    }
+  }
+}
